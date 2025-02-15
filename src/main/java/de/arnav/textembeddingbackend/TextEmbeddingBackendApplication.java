c7fa2cb1188c1ab4e67c857f5df8dcc9307246e1
noir <noir@odin.com>
2025-02-15 16:43:02 +0530

    Intial Commit

---
+++ src/main/java/de/arnav/textembeddingbackend/TextEmbeddingBackendApplication.java
@@ -0,0 +1,13 @@
+package de.arnav.textembeddingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextEmbeddingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextEmbeddingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/arnav/textembeddingbackend/TextEmbeddingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.arnav.textembeddingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextEmbeddingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
